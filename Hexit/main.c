23b1dd338f30ff7a89fc5ddc26abdcb0c004a1a8
Antoxyde <Antoxyde@protonmail.com>
2017-08-01 22:02:58 +0200

    Fix de tout ce qui est input
    On recoit bien tout dans content !

--- Hexit/main.c
+++ Hexit/main.c
@@ -8,36 +8,61 @@
 
 
 
+char *readStreamFromStdin() {
+    char *content = malloc(SIZE);
 
-
-void readStream(FILE *fp, char* data){
-    int c = EOF;
-    unsigned int actualSize = SIZE;
-    unsigned int i = 0;
-    while (( c = getchar()  ) != '\n' && c != EOF){
-        data[i++] = (char)c;
-
-        if (i = actualSize) {
-            actualSize = i + SIZE;
-            data = realloc(data, actualSize);
-        }
+    if (content == NULL) {
+        printf("Unable to malloc.\n");
+        exit(1);
     }
-    fclose(fp);
+
+
+    unsigned int compteur = 0;
+    unsigned int tailleActuelle = SIZE;
+
+    while(1){
+        if (compteur >= tailleActuelle) {
+            tailleActuelle += SIZE;
+            content = realloc(content, tailleActuelle);
+
+            if  (content == NULL) {
+                printf("Unable to realloc.\n");
+                exit(1);
+            }
+        }
+
+        char c = getc(stdin);
+        if (c == EOF || c == '\n') {
+            c = '\0';
+            break;
+        }
+    content[compteur] = c;
+    compteur++;
+    }
+
+    return content;
 }
 
+
+char *readStreamFromFile(FILE *fp) {
+    fseek(fp, 0, SEEK_END);
+    int size = ftell(fp);
+    rewind(fp);
+    char *content = (char*) malloc(sizeof(char) * size);
+    fread(content, 1, size, fp);
+
+    fclose(fp);
+    return content;
+}
 
 
 int main(int argc, char *argv[]) {
 
+    char *content = NULL;
     bool l = false;
     bool r = false;
     int f = -1;
-    char *content = malloc(SIZE);
 
-    if (content == NULL) {
-       printf("Memory allocation failed.\n");
-       exit(1);
-    }
 
     for (int index = 1; index < argc && argv[index][0] == '-'; index++) {
         if (!strcmp(argv[index], "-l")) {
@@ -62,11 +87,13 @@ int main(int argc, char *argv[]) {
     if (f != -1) {
         FILE *fd = fopen(argv[f+1], "r");
         if (fd == NULL) {
-            printf("Impossible de lire le fichier %s.\n" , argv[f+1]);
+            printf("Impossible to open %s.\n" , argv[f+1]);
             exit(1);
         }
-        readStream(fd, content);
+        content = readStreamFromFile(fd);
 
+    } else {
+        content = readStreamFromStdin();
     }
 
 
@@ -81,7 +108,6 @@ int main(int argc, char *argv[]) {
 /*
  -h = l'aide
  -l = little endian
- -r = reverse complet
  -f = on passe un fichier a lire
  -o = fichier d'output
 
